03f73e1e5de331fb22aaf32f7984541667acae8f
malezjaa <66274761+malezjaa@users.noreply.github.com>
2024-01-01 20:52:30 +0100

    chore: remove tsconfig

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,18 +7,5 @@ module.exports = {
     `src/**/*.{js,ts,jsx,tsx}`,
     "../../packages/eddies/src/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        eddies: {
-          bg: "#00ff00",
-
-          dark: {
-            bg: "#00ff00",
-          },
-        },
-      },
-    },
-  },
   plugins: [],
 };
