31d9e03950e87e943af983b9139cf1832b316d1f
malezjaa <66274761+malezjaa@users.noreply.github.com>
2024-01-25 15:28:25 +0100

    ci: test

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Eddies preview",
