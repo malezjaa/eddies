80f645da34d0a49bddd7e7cf77ba6f3a891860bf
malezjaa <66274761+malezjaa@users.noreply.github.com>
2024-01-25 14:18:04 +0100

    fix: add components exports

--- packages/eddies/src/index.tsx
+++ packages/eddies/src/index.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import BubbleMenuComponment from "./components/bubble-menu/bubble-menu";
+import BubbleMenuButton from "./components/bubble-menu/bubble-menu";
+import NodeSelectorComponent from "./components/node-selector/node-selector";
 export * from "./editor";
 export * from "./components/bubble-menu/bubble-menu";
 export * from "./components/bubble-menu/bubble-button";
@@ -8,3 +11,6 @@ export * from "./hooks/useEditor";
 export * from "./types";
 export { EditorContent } from "@tiptap/react";
 export { EddiesEditor } from "@eddieseditor/core";
+export const BubbleMenu = BubbleMenuComponment;
+export const BubbleButton = BubbleMenuButton;
+export const NodeSelector = NodeSelectorComponent;
